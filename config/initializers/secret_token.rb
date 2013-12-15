7c078cdabfe87e33dafaeb05f205352f89ca90d8
Deepak Verma <deepak.23.verma@gmail.com>
2013-12-15 14:44:14 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AlbertaBeach::Application.config.secret_key_base = '33b81e8cc8ce3fa0187fb172ebbc13089f206d37b69b7a3e5c8d1448f858872dd7c177bc91f7ee29dc207b69bf28d21842a9e7b22b7ecf8c65255579bb3bcf1f'
