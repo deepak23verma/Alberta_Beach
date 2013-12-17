d0142cb6c23e88b7ebdd7daf106bda8a347fecdb
Deepak Verma <deepak.23.verma@gmail.com>
2013-12-17 08:29:29 -0500

    generate Registrants controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 AlbertaBeach::Application.routes.draw do
+  get "registrants" => "registrants#index"
+end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +56,4 @@ AlbertaBeach::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
